f32845031c1c11264c5f4ee0739ceda1e06a5bdc
bradd <bradd@gamma.tech>
2026-01-16 18:34:22 +0000

    File changes

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { base44 } from '@/api/base44Client';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { createPageUrl } from '../utils';
 import { 
   Building2, 
