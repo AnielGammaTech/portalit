18ff55a835ec10229af50554d13d0906f368f335
bradd <bradd@gamma.tech>
2026-01-16 18:36:43 +0000

    File changes

--- src/pages/CustomerDetail.jsx
+++ src/pages/CustomerDetail.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { base44 } from '@/api/base44Client';
+import { toast } from 'sonner';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '../utils';
 import { 
