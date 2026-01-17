f3e6c28769c1ae4ed00a1c6c1fbc9fb8b3d6ecd3
bradd <bradd@gamma.tech>
2026-01-17 17:43:20 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,20 +1,14 @@
-import Contracts from './pages/Contracts';
 import CustomerDetail from './pages/CustomerDetail';
-import Customers from './pages/Customers';
 import Dashboard from './pages/Dashboard';
 import Integrations from './pages/Integrations';
-import SaaSManagement from './pages/SaaSManagement';
 import Settings from './pages/Settings';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "Contracts": Contracts,
     "CustomerDetail": CustomerDetail,
-    "Customers": Customers,
     "Dashboard": Dashboard,
     "Integrations": Integrations,
-    "SaaSManagement": SaaSManagement,
     "Settings": Settings,
 }
 
