b185f5ca74b35c2edf49f05016f62e3e546fe1bf
bradd <bradd@gamma.tech>
2026-02-09 21:39:46 +0000

    File changes

--- src/pages/CustomerDetail.jsx
+++ src/pages/CustomerDetail.jsx
@@ -1083,7 +1083,7 @@ export default function CustomerDetail() {
                     }
                     
                     return (
-                      <div className="p-3 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2">
+                      <div className="p-4 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3">
                         {nonJumpcloudApps
                           .filter(([_, data]) => {
                             // Apply category filter
